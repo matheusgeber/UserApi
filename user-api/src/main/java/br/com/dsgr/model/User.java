f856943b77d594cd043323eaf24d839ffac70462
Diego <diegogeber@gmail.com>
2024-07-25 05:43:42 -0300

    Update User.java

--- user-api/src/main/java/br/com/dsgr/model/User.java
+++ user-api/src/main/java/br/com/dsgr/model/User.java
@@ -24,7 +24,13 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String name;
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private String cpfCnpj; 
 	
 	private String password;
 	
